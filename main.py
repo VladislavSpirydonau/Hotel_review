40e1c494d1435dbeef032eb0698b8f13c4028fd8
VladislavSpirydonau <162104721+VladislavSpirydonau@users.noreply.github.com>
2024-03-30 10:48:59 +0100

    optimizing the model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import model
-country = input('Please chose a country for your destination: Netherlands, UK, France, Spain, Italy, Austria \n')
-expectation = input('Please write what do you expect from your trip: \n')
-model.hotel_chosing(country, expectation)
+while True:
+    country = input('Please chose a country for your destination: Netherlands, UK, France, Spain, Italy, Austria \n')
+    expectation = input('Please write what do you expect from your trip: \n')
+    model.hotel_chosing(country, expectation)
--- model.py
+++ model.py
@@ -5,42 +5,41 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import path
 
-# Read CSV file 
-df = pd.read_csv(path.hotel_csv_path)
+# Read CSV file, and on the optimization step i decieded to leave only nescesary columns
+
+use_cols = ['Hotel_Address', 'Tags', 'Average_Score']
+df = pd.read_csv(path.hotel_csv_path, usecols=use_cols)
 # Preprocessing hotel addresses and creating a new column country for adding posibility to client to chose destination
 df.Hotel_Address = df.Hotel_Address.str.lower()
 df.Hotel_Address = df.Hotel_Address.str.replace('united kingdom','uk')
-df['Country'] = df['Hotel_Address'].apply(lambda x: x.split()[-1] if len(x.split()) > 1 else x)
-# Here I remove unnescesary columns
-df1 = df.drop(columns=['Unnamed: 0', 'Additional_Number_of_Scoring', 'Review_Date','Reviewer_Nationality','days_since_review', 'lat','lng','Review_Total_Negative_Word_Counts','Review_Total_Positive_Word_Counts','Total_Number_of_Reviews_Reviewer_Has_Given','Reviewer_Score','Total_Number_of_Reviews', 'Negative_Review','Positive_Review'])
-# Replace unnescesary columns
+df['Country'] = df['Hotel_Address'].str.split().str[-1]
+
+# Repmoove unnescesary symbols
 replace_dict = {"'": '', "[": '', "]": '', ",": ''}
 for pattern, replacement in replace_dict.items():
-    df1['Tags'] = df1['Tags'].str.replace(pattern, replacement)
-df1.Tags = df1.Tags.str.lower()
+    df['Tags'] = df['Tags'].str.replace(pattern, replacement)
+df.Tags = df.Tags.str.lower()
 
 # Initialize the lemmatizer
 lemmatizer = WordNetLemmatizer()
 
 # Tokenize and lemmatize the tags
-df1['Tags'] = df1['Tags'].apply(lambda x: [lemmatizer.lemmatize(word) for word in word_tokenize(x)])
+df['Tags'] = df['Tags'].apply(lambda x: [lemmatizer.lemmatize(word) for word in word_tokenize(x)])
 
 # defining final function
 def hotel_chosing(country, expectation, top_n=5):
-    df_country = df1[df1['Country'] == country.lower()]
+    df_country = df[df['Country'] == country.lower()]
     
     # Tokenize and lemmatize the description
-    description_tokens = [lemmatizer.lemmatize(word) for word in word_tokenize(expectation) if not word in stopwords.words('english')]
-    
-    # Create a mask for rows where the Tags column contains any of the words in the description
-    mask = df_country['Tags'].apply(lambda x: any(word in x for word in description_tokens))
+    description_tokens = set([lemmatizer.lemmatize(word) for word in word_tokenize(expectation) if not word in stopwords.words('english')])
+    mask = df_country['Tags'].apply(lambda x: bool(set(x) & description_tokens))
     
     # Filter the dataframe using the mask
     df_description = df_country[mask]
     #filtering duplicate columns
-    df_description.drop(columns=['Tags', 'Country'], inplace=True)
-    df_description.drop_duplicates(inplace=True)
+    df_description = df_description.drop(columns=['Tags', 'Country'])
+    df_description = df_description.drop_duplicates()
     # Sort the dataframe based on the Average_Score and get the top_n hotels
-    top_hotels = df_description.sort_values('Average_Score', ascending=False).head(top_n)
+    top_hotels = df_description.nlargest(top_n, 'Average_Score')
     
     return print(top_hotels)
